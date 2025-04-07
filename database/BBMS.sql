211734b22198c630c38f22a09ba761a14a9fc38f
ARDHRAWINSTON <ardhraalainaw@gmail.com>
2025-04-07 19:55:47 +0530

    Update BBMS.sql

--- database/BBMS.sql
+++ database/BBMS.sql
@@ -1,11 +1,6 @@
  -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Nov 19, 2023 at 04:10 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
