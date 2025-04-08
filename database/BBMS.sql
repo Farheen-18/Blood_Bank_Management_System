8781473222ccc08e3ccc712025cc3a898b134b12
Farheen <143966590+Farheen-18@users.noreply.github.com>
2025-04-08 10:22:08 +0530

    Update BBMS.sql

--- database/BBMS.sql
+++ database/BBMS.sql
@@ -55,7 +55,7 @@ CREATE TABLE `blood` (
 -- Dumping data for table `blood`
 --
 
---INSERT INTO `blood` (`id`, `AP`, `AN`, `BP`, `BN`, `ABP`, `ABN`, `OP`, `ON`) VALUES
+-- INSERT INTO `blood` (`id`, `AP`, `AN`, `BP`, `BN`, `ABP`, `ABN`, `OP`, `ON`) VALUES
 -- (1, 0, 0, 0, 0, 0, 0, 0, 0);
 -- --------------------------------------------------------
 
