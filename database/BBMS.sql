7b2920ba8a990e4204d24713dd168d99f9a8cf97
Farheen <143966590+Farheen-18@users.noreply.github.com>
2025-04-08 10:29:51 +0530

    Update BBMS.sql

--- database/BBMS.sql
+++ database/BBMS.sql
@@ -77,8 +77,8 @@ CREATE TABLE `donate` (
 -- Dumping data for table `donate`
 --
 
-INSERT INTO `donate` (`id`, `donor_id`, `username`, `disease`, `blood`, `unit`, `status`) VALUES
- (1, 1, 'farheen_18', 'nothing', 'B-', 1, 'approved');
+-- INSERT INTO `donate` (`id`, `donor_id`, `username`, `disease`, `blood`, `unit`, `status`) VALUES
+ -- (1, 1, 'farheen_18', 'nothing', 'B-', 1, 'approved');
 
 -- --------------------------------------------------------
 
@@ -99,8 +99,8 @@ CREATE TABLE `donor` (
 -- Dumping data for table `donor`
 --
 
- INSERT INTO `donor` (`id`, `name`, `username`, `email`, `pwd`, `blood`) VALUES
- (1, 'Farheen', 'farheen_18', 'farheen_18@gmail.com', '$2y$10$Kd5ykwrCfU6VIoS/0M5uEey9LgE8331ff8nwAYMnKHk2VLD678OD.', 'B-');
+-- INSERT INTO `donor` (`id`, `name`, `username`, `email`, `pwd`, `blood`) VALUES
+ -- (1, 'Farheen', 'farheen_18', 'farheen_18@gmail.com', '$2y$10$Kd5ykwrCfU6VIoS/0M5uEey9LgE8331ff8nwAYMnKHk2VLD678OD.', 'B-');
 
 -- --------------------------------------------------------
 
@@ -121,8 +121,8 @@ CREATE TABLE `patient` (
 -- Dumping data for table `patient`
 --
 
- INSERT INTO `patient` (`id`, `name`, `username`, `email`, `pwd`, `blood`) VALUES
- (1, 'Farheen', 'farheen_18', 'farheen_18@gmail.com', '$2y$10$CKRsoSpPcEk7kGBNVNP7xuND9FhVLqLDdkEGCxkqO3uoghC5zz9Ha', 'B+');
+-- INSERT INTO `patient` (`id`, `name`, `username`, `email`, `pwd`, `blood`) VALUES
+ -- (1, 'Farheen', 'farheen_18', 'farheen_18@gmail.com', '$2y$10$CKRsoSpPcEk7kGBNVNP7xuND9FhVLqLDdkEGCxkqO3uoghC5zz9Ha', 'B+');
 
 -- --------------------------------------------------------
 
@@ -144,8 +144,8 @@ CREATE TABLE `request` (
 -- Dumping data for table `request`
 --
 
- INSERT INTO `request` (`id`, `patient_id`, `username`, `reason`, `blood`, `unit`, `status`) VALUES
- (1, 1, 'farheen_18', 'low blood levels', 'B+', 2, 'approved');
+ -- INSERT INTO `request` (`id`, `patient_id`, `username`, `reason`, `blood`, `unit`, `status`) VALUES
+ -- (1, 1, 'farheen_18', 'low blood levels', 'B+', 2, 'approved');
 
 --
 -- Indexes for dumped tables
@@ -203,7 +203,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `blood`
 --
 ALTER TABLE `blood`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 --
 -- AUTO_INCREMENT for table `donate`
@@ -221,7 +221,7 @@ ALTER TABLE `donor`
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 --
 -- AUTO_INCREMENT for table `request`
